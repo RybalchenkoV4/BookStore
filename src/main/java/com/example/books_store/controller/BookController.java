4442257a43397fb28026dde6b7c51ee6877b6ec9
Rybalchenko Vladimir <rybalchenko.v4@gmail.com>
2025-02-16 14:26:54 +0300

    corrected lombok annotations and connected H2

--- src/main/java/com/example/books_store/controller/BookController.java
+++ src/main/java/com/example/books_store/controller/BookController.java
@@ -2,6 +2,7 @@ package com.example.books_store.controller;
 
 import com.example.books_store.model.Book;
 import com.example.books_store.service.BookService;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequiredArgsConstructor
+@AllArgsConstructor
 @RequestMapping("/books")
 public class BookController {
 
--- src/main/java/com/example/books_store/service/BookService.java
+++ src/main/java/com/example/books_store/service/BookService.java
@@ -2,6 +2,7 @@ package com.example.books_store.service;
 
 import com.example.books_store.model.Book;
 import com.example.books_store.repository.BookRepository;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +10,9 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class BookService {
+
     private final BookRepository repository;
 
     public List<Book> findAll() {
